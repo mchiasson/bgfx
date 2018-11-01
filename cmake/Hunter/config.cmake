5cc10a4c1cd21b5806bce1646bb82839ccc58dfa
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-11-01 18:02:07 -0400

    finished hunterizing bgfx

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,9 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p0.tar.gz
-    SHA1 54127bd568fe70610744e25f3fa7752ccbfbe96c
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p1.tar.gz
+    SHA1 0f16cf582faa091165941697cf3499b06af0318e
 )
 
 hunter_config(bimg
-    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p0.tar.gz
-    SHA1 a7e5f84fdcbfb67ee97be6cba586e24e9af99e5c
+    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p1.tar.gz
+    SHA1 0e578dd65ce1bdba09ab5e88425e0944563e0ecf
 )
--- src/renderer_gl.h
+++ src/renderer_gl.h
@@ -56,7 +56,7 @@
 #			include <GL/gl.h>
 #		endif // BX_PLATFORM_
 
-#		include <GL/glext.h>
+#		include <gl/glext.h>
 #	endif // BGFX_CONFIG_RENDERER_OPENGL >= 31
 
 #elif BGFX_CONFIG_RENDERER_OPENGLES
