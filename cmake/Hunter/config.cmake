4617a5851275082e8afd390515633ec8e2505fc3
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-12-08 15:14:49 -0500

    upgraded bgfx to c++14

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,10 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p3.tar.gz
-    SHA1 a3aa9a6d0301651459fad2fed61e4827722d2bb6
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p4.tar.gz
+    SHA1 294ea7afd7b4470613451ffb6e502cab5cc87e4c
 )
 
 hunter_config(bimg
-    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p3.tar.gz
-    SHA1 f9b094f5746c7ec14573bacc3c0bf0d23017add3
+    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p4.tar.gz
+    SHA1 4b35c0f77957930824cb9d4600b048c95f3984ce
 )
+
