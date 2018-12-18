96729f9d9e47633896ff24e44890479a76b62f54
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-12-17 21:29:25 -0500

    upgraded to the latest bx and latest bimg

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,10 +1,9 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p7.tar.gz
-    SHA1 e8735ca5ca598cbce9afec747b205c47ca627836
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p8.tar.gz
+    SHA1 1884503ecfe6570814d191e5176fccc44e16db60
 )
 
 hunter_config(bimg
-    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p7.tar.gz
-    SHA1 2e2a2f3895a2da1123116efc68615b3dc764730c
+    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p8.tar.gz
+    SHA1 d845316761d4276066eb05fe7b7aa47af59c9531
 )
-
