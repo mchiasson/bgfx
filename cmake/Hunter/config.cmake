0aa1ca3002e63054f82661f59af4b4f802368f14
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-01-16 17:57:21 -0500

    hunterized

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,10 @@
+hunter_config(bx
+    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-p0.tar.gz
+    SHA1 a9aa1a49c8ef6c87ab9aa989852806330e074fb2
+)
+
+hunter_config(bimg
+    URL https://github.com/YOU-i-Labs/bimg/archive/v1.0.0-p0.tar.gz
+    SHA1 a65f311bc0b6aae36c558f49d765a8a6b63cca8c
+)
+
