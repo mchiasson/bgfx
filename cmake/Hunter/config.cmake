bf023e0c5d5cc68df241170222d41afd95df1745
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-02-21 18:39:35 -0500

    fixed build after pulling the latest

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,9 @@
 hunter_config(bx
-    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-1.tar.gz
-    SHA1 1dcecc9347d3216c0103ddbc821c29453be6f0ce
+    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-2.tar.gz
+    SHA1 42e1587395798c13e440da2437a32bcba701e7c8
 )
 
 hunter_config(bimg
-    URL https://github.com/YOU-i-Labs/bimg/archive/v1.0.0-youi-1.tar.gz
-    SHA1 7a075e4d4aa075ea921967a4da26567517bcae4c
+    URL https://github.com/YOU-i-Labs/bimg/archive/v1.0.0-youi-2.tar.gz
+    SHA1 95e185e21595729b144fca0eba141344c9e52b9d
 )
