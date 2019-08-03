d39e325edf5b60c90faa684e74115f396cc1bd98
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-08-03 17:06:38 -0400

    upgraded bx and bimg to the latest

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,9 @@
 hunter_config(bx
-    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-3.tar.gz
-    SHA1 a2ff4699207214af9673741d6c42314eb7be09a8
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p9.tar.gz
+    SHA1 8f40ef60c98e6a1217b1f0f1340aa82336d1ef24
 )
 
 hunter_config(bimg
-    URL https://github.com/YOU-i-Labs/bimg/archive/v1.0.0-youi-3.tar.gz
-    SHA1 01318d07324b00715fc582e4da5833843e2d67bb
+    URL https://github.com/mchiasson/bimg/archive/v1.0.0-p9.tar.gz
+    SHA1 1ad67e6c2929539997fba98f0532f5a87153583a
 )
